a22617eca6b2cffb4791e1518c2f48628baef9c1
Helliarc 1 <helliarc@hotmail.com>
2019-03-11 05:36:40 -0500

    Note added on line 10.

--- Project1.10/Project1.10.cpp
+++ Project1.10/Project1.10.cpp
@@ -6,6 +6,8 @@ int main()
 	int usrNum{}; //variable usrNum for the user's entered number
 	std::cin >> usrNum; //stores user's entered number to variable usrNum
     std::cout << "Your integer doubled is: " << usrNum * 2; //outputs the usrNum variable doubled.
+
+	//added note to test GitHub.
 	   
 	return 0;
 }
