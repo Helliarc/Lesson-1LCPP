764fc65158da22867c71d6efd8ab2624a6ab6965
Helliarc 1 <helliarc@hotmail.com>
2019-03-11 05:24:03 -0500

    Add project files.

---
+++ Project1.10/Project1.10.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+	std::cout << "Enter an integer: ";
+	int usrNum{}; //variable usrNum for the user's entered number
+	std::cin >> usrNum; //stores user's entered number to variable usrNum
+    std::cout << "Your integer doubled is: " << usrNum * 2; //outputs the usrNum variable doubled.
+	   
+	return 0;
+}
